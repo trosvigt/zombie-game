70df50451f5e9954d7e11cdeb83cd80460b9f5e4
Tristan <ttrosvig804@gmail.com>
2023-06-09 18:43:43 -0500

    Added logic to randomly generate Characters

--- Game/src/main/java/com/mycompany/game/Game.java
+++ Game/src/main/java/com/mycompany/game/Game.java
@@ -4,17 +4,113 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Game {
-
     private static Random random = new Random();
-    private ArrayList<Character> characters;
+    private static ArrayList<Character> characters;
     private static int survivorCount;
     private static int zombieCount;
 
     public static void main(String[] args) {
         // Generate random Character list
-        
-        displayStats();
+        characters = generateRandomCharacters();
+        System.out.println(characters.size());
 
+        displayStats();
+    }
+
+    // This method will generate a random list of characters. The
+    // number of characters generated is random, along with the
+    // number of zombies and survivors
+    private static ArrayList<Character> generateRandomCharacters() {
+        // Declarations for random generation
+        int characterType;
+        int zombie;
+        int survivor;
+
+        // List to hold characters
+        ArrayList<Character> characters = new ArrayList();
+        
+        // Get the total character count
+        // Default min is 10 and default max is 30
+        int characterCount = getRandomNumber(10, 31);
+
+        // Generate number of characters equal to characterCount
+        for (int i = 0; i < characterCount; i++) {
+            // Generate one or two to pick between Survivor and Zombie
+            characterType = getRandomNumber(1, 3);
+
+            // Depending on the type...
+            switch(characterType) {
+                // Zombie case
+                case 1: {
+                    // Generate one or two to pick between common
+                    // infected and tank, respectively
+                    zombie = getRandomNumber(1, 3);
+
+                    // Depending on zombie...
+                    switch (zombie) {
+                        // Common infected case
+                        case 1: {
+                            // Create common infected
+                            characters.add(new CommonInfected());
+                            break;
+                        }
+                        // Tank case
+                        case 2: {
+                            // Create tank
+                            characters.add(new Tank());
+                            break;
+                        }
+                        default: {
+                            System.out.println("Unable to create Zombie...");
+                            break;
+                        }
+                    }
+                    break;
+                }
+                // Survivor case
+                case 2: {
+                    // Generate one, two, or three to pick between civilian,
+                    // scientist, and soldier, respectively
+                    survivor = getRandomNumber(1, 4);
+
+                    // Depending on the survivor...
+                    switch(survivor) {
+                        case 1: {
+                            // Create civilian
+                            characters.add(new Civilian());
+                            break;
+                        }
+                        case 2: {
+                            // Create scientist
+                            characters.add(new Scientist());
+                            break;
+                        }
+                        case 3: {
+                            characters.add(new Soldier());
+                            break;
+                        }
+                        default: {
+                            System.out.println("Unable to create Survivor...");
+                            break;
+                        }
+                    }
+                    break;
+                }
+                // Invalid number
+                default: {
+                    System.out.println("Unable to create Character...");
+                    break;
+                }
+            }
+        }
+
+        return characters;
+    }
+
+    // This method will return a random int between
+    // low (inclusive) and high (exclusive)
+    private static int getRandomNumber(int low, int high) {
+        return random.nextInt(high - low) + low;
     }
 
     // This method will contain the main battle logic...
@@ -30,7 +126,5 @@ public class Game {
         System.out.println("We have " + survivorCount + " survivors trying to make it to safety.\n");
         System.out.println("But there are " + zombieCount + " zombies waiting for them.\n");
         System.out.println("It seems " + 0 + " have made it to safety.");
-
     }
-
 }
